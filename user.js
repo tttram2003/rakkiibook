bafc4c1c24610ca2b4752fccbced2fc819a39bb0
tttram2003 <95835925+tttram2003@users.noreply.github.com>
2024-01-10 21:11:47 +0700

    Update user.js

--- user.js
+++ user.js
@@ -66,6 +66,6 @@ app.post('/update-password', function(req, res) {
     });
 });
 
-app.listen(3000, function() {
-    console.log('App listening on port 3000');
-});
+// app.listen(3000, function() {
+//     console.log('App listening on port 3000');
+// });
